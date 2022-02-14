674ae7ada0d088f10b14f797ae58d213196b1d41
Kirill Tobola <kirilltobola@gmail.com>
2022-02-14 22:11:22 +0800

    add web security config

---
+++ src/main/java/isu/volunteer/api/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package isu.volunteer.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import isu.volunteer.api.jwt.Configurer;
+import isu.volunteer.api.jwt.TokenUtil;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private TokenUtil tokenProvider;
+
+    @Autowired
+    public WebSecurityConfig(TokenUtil tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/v1/auth/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/v1/orders").hasRole("MEDIC")
+            .anyRequest().authenticated()
+            .and()
+            .apply(new Configurer(this.tokenProvider));
+    }
+}
