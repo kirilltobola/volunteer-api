fa88d329e9886a0e26c84e958f0bd23aa383dcb0
Kirill Tobola <kirilltobola@gmail.com>
2022-02-14 22:10:54 +0800

    add password encoder bean

--- src/main/java/isu/volunteer/api/ApiApplication.java
+++ src/main/java/isu/volunteer/api/ApiApplication.java
@@ -2,9 +2,18 @@ package isu.volunteer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
+	@Bean
+	@Primary
+	public BCryptPasswordEncoder getpce() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
