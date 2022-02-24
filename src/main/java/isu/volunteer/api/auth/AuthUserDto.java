1a3c3f2a85f5f85030cca9ee144572bcf6875c60
Kirill Tobola <kirilltobola@gmail.com>
2022-02-25 05:25:44 +0800

    add auth dto

--- src/main/java/isu/volunteer/api/auth/AuthUserDto.java
+++ src/main/java/isu/volunteer/api/auth/AuthUserDto.java
@@ -1,9 +1,16 @@
 package isu.volunteer.api.auth;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class AuthUserDto {
+    @NotBlank
     private String username;
+
+    @NotBlank
+    @Size(min = 8)
     private String password;
 }
