b8afa1122684e8fb6417b4dd7d04e882490774d3
Kirill Tobola <kirilltobola@gmail.com>
2022-02-25 05:29:40 +0800

    add auth controller

--- src/main/java/isu/volunteer/api/auth/AuthController.java
+++ src/main/java/isu/volunteer/api/auth/AuthController.java
@@ -3,11 +3,13 @@ package isu.volunteer.api.auth;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,34 +17,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import isu.volunteer.api.jwt.TokenUtil;
 import isu.volunteer.api.user.User;
+import isu.volunteer.api.user.UserFactory;
 import isu.volunteer.api.user.UserService;
 
 
 @RestController
 @RequestMapping(value = "/api/v1/auth")
+@Validated
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
-
-    private final TokenUtil tokenProvider;
-
-    private final UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController (AuthenticationManager authenticationManager, TokenUtil tokenProvider, UserService userService) {
-        this.authenticationManager = authenticationManager;
-        this.tokenProvider = tokenProvider;
-        this.userService = userService;
-    }
+    private TokenUtil tokenProvider;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserFactory userFactory;
+
 
     @PostMapping(path = "/register")
-    public ResponseEntity register(@RequestBody AuthUserDto userDto) {
-        User user = new User();
+    public ResponseEntity<Object> register(@RequestBody @Valid AuthUserDto userDto) {
+        User user = this.userFactory.create();
         user.setUsername(userDto.getUsername());
         user.setPassword(userDto.getPassword());
-        this.userService.add(user);
 
+        this.userService.register(user);
+
+        Map<Object, Object> response = new HashMap<>();
         String token = this.tokenProvider.createToken(userDto.getUsername(), user.getRoles());
-        Map<String, Object> response = new HashMap<>();
         response.put("username", userDto.getUsername());
         response.put("token", token);
         response.put("expires in", this.tokenProvider.getExpirationByToken(token));
@@ -51,15 +56,12 @@ public class AuthController {
     }
     
     @PostMapping(path = "/login")
-    public ResponseEntity login(@RequestBody AuthUserDto userDto) {
+    public ResponseEntity<Object> login(@RequestBody @Valid AuthUserDto userDto) {
         String username = userDto.getUsername();
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, userDto.getPassword()));
         User user = userService.findByUsername(username);
 
-        if (user == null) {
-            throw new UsernameNotFoundException("not found");
-        }
-        Map<String, Object> response = new HashMap<>();
+        Map<Object, Object> response = new HashMap<>();
         response.put("username", username);
 
         String token = this.tokenProvider.createToken(username, user.getRoles());
